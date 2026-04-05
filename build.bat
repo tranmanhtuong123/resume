aa4181f86fa6ae2202046bd7343dd8f3ad3b0034
tuong.tm <tuongtran1345@gmaill.com>
2026-04-05 19:10:06 +0700

    update

--- build.bat
+++ build.bat
@@ -2,6 +2,12 @@
 
 docker build -t tuongtm/latex . 
 
-docker run --rm -i -v %cd%:/data tuongtm/latex pdflatex tex/tuongtm_resume.tex
+@REM docker run --rm -i -v %cd%:/data tuongtm/latex pdflatex tex/draft/2025.tex
+
+@REM docker run --rm -i -v %cd%:/data tuongtm/latex pdflatex tex/draft/2026.tex
+
+@REM docker run --rm -i -v %cd%:/data tuongtm/latex pdflatex tex/resume.tex
+
+docker run --rm -i -v %cd%:/data tuongtm/latex pdflatex tex/TranManhTuong-BankednDeveloper.tex
 
 pause
